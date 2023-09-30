28b6063c6c2c8f526d6fb14c7fa699bf5ff9ec21
Marius Alban <malban298@gmail.com>
2023-09-30 11:38:25 -0300

    finished

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function createTimeOutEvent(employeeRecord, dateStamp){
     return employeeRecord
 }
 
-function hoursWorkedOnDate(employeeRecord){
+function hoursWorkedOnDate(employeeRecord, date){
 let hourIn;
 let hourOut;
 let timeIn = employeeRecord.timeInEvents;
@@ -57,53 +57,12 @@ let timeOut = employeeRecord.timeOutEvents;
     hourOut = timeOut[keyOut].hour
     hourOut = hourOut/100}
     return hourOut - hourIn
-    }
-function wagesEarnedOnDate(employeeRecord, date){
-    let hourIn;
-    let hourOut;
-    let totalHours;
-    let timeIn = employeeRecord.timeInEvents;
-    let timeOut = employeeRecord.timeOutEvents;
-        for(let keyIn in timeIn){
-            if (date = timeIn[keyIn].date){
-            hourIn = timeIn[keyIn].hour
-            hourIn = hourIn/100}}
-        for(let keyOut in timeOut){
-            hourOut = timeOut[keyOut].hour
-            hourOut = hourOut/100}
-        totalHours = hourOut - hourIn
-return totalHours * employeeRecord.payPerHour
-            }
+}
 
-function allWagesFor(employeeRecord){
- let hourIn1;
-    let hourOut1;
-    let dayOneHours;
-    let timeIn1 = employeeRecord.timeInEvents[0];
-    let timeOut1 = employeeRecord.timeOutEvents[0];
-        for(let keyIn1 in timeIn1){
-        hourIn1 = timeIn1[keyIn1].hour
-        hourIn1 = hourIn1/100}
-        for(let keyOut1 in timeOut1){
-        hourOut1 = timeOut1[keyOut1].hour
-        hourOut1 = hourOut1/100}
-    dayOneHours = hourOut1 - hourIn1;
     
-    let hourIn2;
-    let hourOut2;
-    let dayTwoHours;
-    let timeIn2 = employeeRecord.timeInEvents[1];
-    let timeOut2 = employeeRecord.timeOutEvents[1];
-        for(let keyIn2 in timeIn2){
-        hourIn2 = timeIn2[keyIn2].hour
-        hourIn2 = hourIn2/100}
-        for(let keyOut2 in timeOut2){
-        hourOut2 = timeOut2[keyOut2].hour
-        hourOut2 = hourOut2/100}
-    dayTwoHours = hourOut2 - hourIn2
-    let totalHours = dayOneHours + dayTwoHours;
-    return totalHours * employeeRecord.payPerHour
-     }
+
+
+
 
  function wagesEarnedOnDate(employeeRecord, date){
 let hourIn;
@@ -122,26 +81,40 @@ let timeOut = employeeRecord.timeOutEvents;
     console.log(employeeRecord)
 return totalHours * employeeRecord.payPerHour
  }
+
+ function allWagesFor(employeeRecord){
+    let wagesArray = []
+
+    employeeRecord.timeInEvents.forEach((event, n) => {
+        let timeOut = employeeRecord.timeOutEvents[n].hour
+        let timeIn = event.hour
+        let hoursWorked = (timeOut-timeIn)/100
+        wagesArray.push(hoursWorked * employeeRecord.payPerHour )
+    })
+
+    return wagesArray.reduce((a, b) => a + b)
+}
+ 
         
         
-function allWagesFor(employeeRecord){
- wageEarned = []
-      
- employeeRecord.timeInEvents.forEach(event => wageEarned.push(wagesEarnedOnDate(event.hour)))
+
+
+
+
+function calculatePayroll(employeeRecords){
+
+    let payroll = []
+
+    // employeeRecords.forEach(employee => payroll.push(allWagesFor(employee)))
+    employeeRecords.forEach(employeeRecord => {
+        let wageOfEmployee = allWagesFor(employeeRecord)
+        payroll.push(wageOfEmployee)
+    }) 
+
+    let allWages = payroll.reduce((a, b) => a + b)
+
+    return allWages
+
+    // return payroll.reduce((a, b) => a + b)
     
-    
- employeeRecord.timeOutEvents.forEach(event => wageEarned.push(wagesEarnedOnDate(event.hour)))
-      
- console.log(wageEarned)
-return wageEarned
 }
-
-
-
-function calculatePayroll(){
-
-
-
-
-}
-
